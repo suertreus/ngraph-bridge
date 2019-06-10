27f310f0867e6dcc9715cc6ce45c01a9e1a5b84a
Sayantan Sarkar <sayantan.sarkar@intel.com>
2019-06-10 14:28:01 -0700

    Shape inference example

--- test/graph_rewrites/mark_for_clustering_test.cc
+++ test/graph_rewrites/mark_for_clustering_test.cc
@@ -20,6 +20,16 @@
 #include "ngraph_utils.h"
 #include "tensorflow/core/graph/graph.h"
 #include "tensorflow/core/graph/node_builder.h"
+#include "tensorflow/core/grappler/grappler_item.h"
+#include "tensorflow/core/grappler/clusters/cluster.h"
+#include "tensorflow/core/grappler/costs/graph_properties.h"
+#include "tensorflow/core/grappler/grappler_item_builder.h"
+#include "tensorflow/core/framework/function_testlib.h"
+#include "tensorflow/core/lib/core/status_test_util.h"
+#include "tensorflow/core/platform/test.h"
+#include "tensorflow/core/protobuf/meta_graph.pb.h"
+#include "tensorflow/core/graph/graph_def_builder.h"
+#include "tensorflow/cc/ops/standard_ops.h"
 #include "tf_graph_writer.h"
 
 using namespace std;
@@ -81,6 +91,84 @@ TEST(MarkForClustering, SimpleTest) {
     ASSERT_OK(GetNodeBackend(node, &backend));
     ASSERT_EQ(backend, expected_backend);
   }
+}
+
+//namespace grappler {
+
+//class MarkForClustering : public ::testing::Test {};
+
+TEST(MarkForClustering, ShapeTest) {
+  //using test::function::NDef;
+  //using test::function::GDef;
+  MetaGraphDef meta_graph;
+
+
+// TODO use graphdefbuilder instead of graph->graphdef
+ Graph g(OpRegistry::Global());
+ Tensor t_input_0(DT_FLOAT, TensorShape{2, 3});
+  Tensor t_input_1(DT_FLOAT, TensorShape{2, 3});
+
+  Node* node1;
+  ASSERT_OK(NodeBuilder("node1", "Const")
+                .Attr("dtype", DT_FLOAT)
+                .Attr("value", t_input_0)
+                .Finalize(&g, &node1));
+
+  Node* node2;
+  ASSERT_OK(NodeBuilder("node2", "Abs")
+                .Input(node1, 0)
+                .Attr("T", DT_FLOAT)
+                .Finalize(&g, &node2));
+
+  // Add edges from SRC to node1 and node2
+  // Add edge from node3 to SINK
+  // The graph is disconnected without these edges
+  Node* source = g.source_node();
+  Node* sink = g.sink_node();
+  g.AddEdge(source, Graph::kControlSlot, node1, Graph::kControlSlot);
+  g.AddEdge(node2, Graph::kControlSlot, sink, Graph::kControlSlot);
+  GraphDef gdef;
+  g.ToGraphDef(&gdef);
+
+  *meta_graph.mutable_graph_def() = gdef;
+  CollectionDef collection;
+  collection.mutable_node_list()->add_value("node2");
+  (*meta_graph.mutable_collection_def())["train_op"] = collection;
+
+  grappler::ItemConfig cfg;
+  std::unique_ptr<grappler::GrapplerItem> item =
+      GrapplerItemFromMetaGraphDef("0", meta_graph, cfg);
+
+  //grappler::GrapplerItem item;
+  grappler::GraphProperties properties(*item);
+  Status s = properties.InferStatically(true);
+
+  for (const auto& node_name : vector<string>{"node1", "node2"}) {
+    cout << node_name << "\n";
+    auto inp_props = properties.GetInputProperties(node_name);
+    cout << inp_props.size() << "\n";
+    auto out_props = properties.GetOutputProperties(node_name);
+    cout << out_props.size() << "\n";
+
+    for (int ii = 0; ii < inp_props.size(); ii++){
+      const OpInfo::TensorProperties& prop_1 = inp_props[ii];
+      auto sh = prop_1.shape();
+      for (int i = 0; i < sh.dim_size(); i++){
+        cout << sh.dim(i).size() << " -- ";
+      }
+      cout << "\n";
+    }
+    
+    for (int ii = 0; ii < out_props.size(); ii++){
+      const OpInfo::TensorProperties& prop_1 = out_props[ii];
+      auto sh = prop_1.shape();
+      for (int i = 0; i < sh.dim_size(); i++){
+        cout << sh.dim(i).size() << " -- ";
+      }
+      cout << "\n";
+    }
+  }
+//}
 }
 }
 }
